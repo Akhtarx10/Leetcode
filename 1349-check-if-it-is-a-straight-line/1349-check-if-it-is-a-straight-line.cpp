7129283cd2a69f7a3d0ad4c75d2583c27cbfd8ac
Shoaib Akhtar <97195224+Akhtarx11@users.noreply.github.com>
2023-09-28 20:47:32 +0530

    Time: 7 ms (71.64%), Space: 10.5 MB (15.97%) - LeetHub

--- 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.cpp
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.cpp
@@ -3,7 +3,7 @@ public:
     bool checkStraightLine(vector<vector<int>>& coordinates) {
         int n = coordinates.size();
         if (n <= 2) {
-            return true; // Any two points or fewer are always on a straight line.
+            return true; 
         }
 
         for (int i = 2; i < n; i++) {
@@ -11,7 +11,7 @@ public:
             int x1 = coordinates[i - 1][0], y1 = coordinates[i - 1][1];
             int x2 = coordinates[i][0], y2 = coordinates[i][1];
             
-            // Check if the points are collinear.
+           
             if ((y1 - y0) * (x2 - x1) != (y2 - y1) * (x1 - x0)) {
                 return false;
             }
