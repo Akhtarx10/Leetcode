be7021ca23274cbb7be9bb24a29eb2ba14a37174
Shoaib Akhtar <97195224+Akhtarx11@users.noreply.github.com>
2023-10-03 20:52:39 +0530

    Time: 3 ms (27.96%), Space: 6.9 MB (5.12%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,11 +1,26 @@
+#include <vector>
+
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-       long num=1;
-        for(int i=1;i<m;i++){
-            num= num*(n-1+i)/i;
+        
+        std::vector<std::vector<int>> dp(m, std::vector<int>(n, 0));
+
+       
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
         }
-        return (int)num;
-    }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+     }
     
 };
