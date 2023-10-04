53d2f16c8497d3f38fb1906cfa6b386da0bec266
Shoaib Akhtar <97195224+Akhtarx11@users.noreply.github.com>
2023-10-04 20:54:57 +0530

    Time: 171 ms (12.87%), Space: 50.7 MB (96.52%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.cpp
@@ -0,0 +1,39 @@
+class MyHashMap {
+public:
+    vector<pair<int, int>> data;
+    
+    /** Initialize your data structure here. */
+    MyHashMap() {
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        for (auto &entry : data) {
+            if (entry.first == key) {
+                entry.second = value;
+                return;
+            }
+        }
+        data.push_back(make_pair(key, value));
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        for (const auto &entry : data) {
+            if (entry.first == key) {
+                return entry.second;
+            }
+        }
+        return -1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        for (auto it = data.begin(); it != data.end(); ++it) {
+            if (it->first == key) {
+                data.erase(it);
+                return;
+            }
+        }
+    }
+};
