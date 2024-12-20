032711c742d250fec27035ee1ea5a2f852cdbaca
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2024-12-21 01:28:23 +0530

    Time: 6 ms (28.09%), Space: 84.1 MB (15.86%) - LeetHub

--- 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.cpp
@@ -12,17 +12,33 @@
 class Solution {
  public:
   TreeNode* reverseOddLevels(TreeNode* root) {
-    dfs(root->left, root->right, true);
-    return root;
-  }
+    queue<TreeNode*> q;
+    q.push(root);
+    bool isOddLevel = false;
 
- private:
-  void dfs(TreeNode* left, TreeNode* right, bool isOddLevel) {
-    if (left == nullptr)
-      return;
-    if (isOddLevel)
-      swap(left->val, right->val);
-    dfs(left->left, right->right, !isOddLevel);
-    dfs(left->right, right->left, !isOddLevel);
+    while (!q.empty()) {
+      int size = q.size();
+      vector<TreeNode*> levelNodes;
+
+      for (int i = 0; i < size; ++i) {
+        TreeNode* node = q.front();
+        q.pop();
+        levelNodes.push_back(node);
+
+        if (node->left) q.push(node->left);
+        if (node->right) q.push(node->right);
+      }
+
+      if (isOddLevel) {
+        int n = levelNodes.size();
+        for (int i = 0; i < n / 2; ++i) {
+          swap(levelNodes[i]->val, levelNodes[n - 1 - i]->val);
+        }
+      }
+
+      isOddLevel = !isOddLevel;
+    }
+
+    return root;
   }
 };
