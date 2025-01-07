5dc89fb20b04fe3a28405e913ddb5ea74a332b29
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-01-07 22:59:51 +0530

    Time: 1 ms (100%), Space: 42.2 MB (73.02%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -1,17 +1,27 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-        Arrays.sort(words, (a, b) -> a.length() - b.length());
-        List<String> ans = new ArrayList<>();
+        List<String> result = new ArrayList<>();
 
         for (int i = 0; i < words.length; i++) {
-            for (int j = i + 1; j < words.length; j++) {
-                if (words[j].contains(words[i])) {
-                    ans.add(words[i]);
-                    break;
-                }
+            String curr = words[i];
+            if (isSubstringOfSomeoneButIt(words, curr, i)) {
+                result.add(curr);
+            } 
+        }
+
+        return result;
+    }
+
+    private static boolean isSubstringOfSomeoneButIt(String[] words, String curr, int indexToExclude) {
+        for (int j = 0; j < words.length; j++) {
+            if (j == indexToExclude)
+                continue;
+            String other = words[j];
+            if (other.contains(curr)) { // TODO is null allowed ?
+                return true;
             }
         }
 
-        return ans;
+        return false;
     }
 }
