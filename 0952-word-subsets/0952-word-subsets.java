a19dc1c428ec8f1deca27d9f02a6107927a7189b
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-01-10 22:47:58 +0530

    Time: 13 ms (81.4%), Space: 55.1 MB (20.35%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.java
+++ 0952-word-subsets/0952-word-subsets.java
@@ -1,34 +1,32 @@
 class Solution {
-    public List<String> wordSubsets(String[] A, String[] B) {
-       List<String> result=new ArrayList<>();
-        int[] target=new int[26];
-        
-        for(String word:B){
-            int[] temp=new int[26];
-            for(char ch:word.toCharArray()){
-                temp[ch-'a']++;
-                target[ch-'a']=Math.max(target[ch-'a'],temp[ch-'a']);
-            }
-        }
-        
-        for(String word:A){
-            int[] source=new int[26];
-            for(char ch:word.toCharArray()){
-                source[ch-'a']++;
-            }
-            
-            if(subset(source,target)){
-                result.add(word);
-            }
-        }
-        
-        return result;
+  public List<String> wordSubsets(String[] A, String[] B) {
+    List<String> ans = new ArrayList<>();
+    int[] countB = new int[26];
+
+    for (final String b : B) {
+      int[] temp = counter(b);
+      for (int i = 0; i < 26; ++i)
+        countB[i] = Math.max(countB[i], temp[i]);
     }
-    
-    private boolean subset(int[] parent,int[] child){
-        for(int i=0;i<26;i++){
-            if(parent[i]<child[i]) return false;
-        }
-        return true;
-    }
+
+    for (final String a : A)
+      if (isUniversal(counter(a), countB))
+        ans.add(a);
+
+    return ans;
+  }
+
+  private int[] counter(final String s) {
+    int[] count = new int[26];
+    for (char c : s.toCharArray())
+      ++count[c - 'a'];
+    return count;
+  }
+
+  private boolean isUniversal(int[] countA, int[] countB) {
+    for (int i = 0; i < 26; ++i)
+      if (countA[i] < countB[i])
+        return false;
+    return true;
+  }
 }
