b9459465caddbf33a9b9996c10933e0b99e67c57
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-01-23 00:15:29 +0530

    Time: 12 ms (100%), Space: 170.3 MB (24.76%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.java
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.java
@@ -1,34 +1,36 @@
 class Solution {
-  public int[][] highestPeak(int[][] isWater) {
-    final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    final int m = isWater.length;
-    final int n = isWater[0].length;
-    int[][] ans = new int[m][n];
-    Arrays.stream(ans).forEach(A -> Arrays.fill(A, -1));
-    Queue<Pair<Integer, Integer>> q = new ArrayDeque<>();
-
-    for (int i = 0; i < m; ++i)
-      for (int j = 0; j < n; ++j)
-        if (isWater[i][j] == 1) {
-          q.offer(new Pair<>(i, j));
-          ans[i][j] = 0;
+    public int[][] highestPeak(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int maxVal = 2000;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (matrix[r][c] == 1) matrix[r][c] = 0;
+                else matrix[r][c] = 1;
+            }
         }
-
-    while (!q.isEmpty()) {
-      final int i = q.peek().getKey();
-      final int j = q.poll().getValue();
-      for (int[] dir : dirs) {
-        final int x = i + dir[0];
-        final int y = j + dir[1];
-        if (x < 0 || x == m || y < 0 || y == n)
-          continue;
-        if (ans[x][y] != -1)
-          continue;
-        ans[x][y] = ans[i][j] + 1;
-        q.offer(new Pair<>(x, y));
-      }
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (matrix[r][c] != 0) {
+                    int top = maxVal;
+                    int left = maxVal;
+                    if (r - 1 >= 0) top = matrix[r - 1][c];
+                    if (c - 1 >= 0) left = matrix[r][c - 1];
+                    matrix[r][c] = Math.min(top, left) + 1;
+                }
+            }
+        }
+        for (int r = m - 1; r >= 0; r--) {
+            for (int c = n - 1; c >= 0; c--) {
+                if (matrix[r][c] != 0) {
+                    int bottom = maxVal;
+                    int right = maxVal;
+                    if (r + 1 < m) bottom = matrix[r + 1][c];
+                    if (c + 1 < n) right = matrix[r][c + 1];
+                    matrix[r][c] = Math.min(matrix[r][c], Math.min(bottom, right) + 1);
+                }
+            }
+        }
+        return matrix;
     }
-
-    return ans;
-  }
 }
