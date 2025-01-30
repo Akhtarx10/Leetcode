7a0ad79e304189826b9af69fe7662b2ee169bf2c
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-01-30 21:36:47 +0530

    Time: 250 ms (97.62%), Space: 48.1 MB (74.6%) - LeetHub

--- 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.java
+++ 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.java
@@ -1,36 +1,84 @@
 class Solution {
     public int magnificentSets(int n, int[][] edges) {
-        List<Integer>[] g = new List[n];
-        Arrays.setAll(g, k -> new ArrayList<>());
-        for (var e : edges) {
-            int a = e[0] - 1, b = e[1] - 1;
-            g[a].add(b);
-            g[b].add(a);
+        List<List<Integer>> adj = new ArrayList<>();
+        n++;
+        for(int i = 0; i < n; i++)
+            adj.add(new ArrayList<>());
+        for(int edg[] : edges)
+        {
+            adj.get(edg[0]).add(edg[1]);
+            adj.get(edg[1]).add(edg[0]);
         }
-        int[] d = new int[n];
-        int[] dist = new int[n];
-        for (int i = 0; i < n; ++i) {
-            Deque<Integer> q = new ArrayDeque<>();
-            q.offer(i);
-            Arrays.fill(dist, 0);
-            dist[i] = 1;
-            int mx = 1;
-            int root = i;
-            while (!q.isEmpty()) {
-                int a = q.poll();
-                root = Math.min(root, a);
-                for (int b : g[a]) {
-                    if (dist[b] == 0) {
-                        dist[b] = dist[a] + 1;
-                        mx = Math.max(mx, dist[b]);
-                        q.offer(b);
-                    } else if (Math.abs(dist[b] - dist[a]) != 1) {
-                        return -1;
-                    }
+
+        if(!isBipartite(adj, n))
+            return -1;
+
+        int degreeBFS[] = new int[n];
+        for(int i = 1; i < n; i++)
+            degreeBFS[i] = bfsdegree(adj, i);
+
+        int vis[] = new int[n];
+        int grp = 0;
+        for(int i = 1; i < n; i++)
+            if(vis[i] == 0)
+                grp += dfs(adj, vis, degreeBFS, i);
+
+        return grp;
+    }
+
+    public int dfs(List<List<Integer>> adj,int vis[], int deg[], int vertex) {
+        vis[vertex] = 1;
+        int componentMax = deg[vertex];
+        for(int ver : adj.get(vertex))
+            if(vis[ver] == 0)
+                componentMax = Math.max(componentMax, dfs(adj, vis, deg, ver));
+        return componentMax;
+    }
+
+    public int bfsdegree(List<List<Integer>> adj, int vertex) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{vertex, 1});
+        int vis[] = new int[adj.size()];
+        vis[vertex] = 1;
+        int ele[] = new int[2];
+        while(!q.isEmpty())
+        {
+            ele = q.poll();
+            for(int ver : adj.get(ele[0]))
+                if(vis[ver] == 0)
+                {
+                    vis[ver] = 1;
+                    q.add(new int[]{ver, ele[1] + 1});
                 }
-            }
-            d[root] = Math.max(d[root], mx);
         }
-        return Arrays.stream(d).sum();
+        return ele[1];
+    }
+
+    public boolean isBipartite(List<List<Integer>> adj, int n) {
+        int color[] = new int[n];
+        for(int i = 0; i < n; i++)
+            if(color[i] == 0 && !bfs(adj, color, i))
+                return false;
+        return true;
+    }
+
+    public boolean bfs(List<List<Integer>> adj, int color[], int vertex) {
+        color[vertex] = 1;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(vertex);
+        while(!q.isEmpty())
+        {
+            int ele = q.poll();
+            int col = color[ele] == 1 ? 2 : 1;
+            for(int ver : adj.get(ele))
+                if(color[ver] == 0)
+                {
+                    color[ver] = col;
+                    q.add(ver);
+                }
+                else if(color[ver] != col)
+                    return false;
+        }
+        return true;
     }
 }
