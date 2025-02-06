bfd5b0eb76dc84423c8810fee1229796132f7beb
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-02-06 22:38:47 +0530

    Time: 238 ms (42.31%), Space: 68.7 MB (66.03%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int tupleSameProduct(int[] nums) {
+        Map<Integer, Integer> productCount = new HashMap<>();
+        int n = nums.length, count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int product = nums[i] * nums[j];
+                productCount.put(product, productCount.getOrDefault(product, 0) + 1);
+            }
+        }
+
+        for (int freq : productCount.values()) {
+            if (freq > 1) {
+                count += (freq * (freq - 1) / 2) * 8;
+            }
+        }
+
+        return count;
+    }
+}
+
+
+
+
+
+
