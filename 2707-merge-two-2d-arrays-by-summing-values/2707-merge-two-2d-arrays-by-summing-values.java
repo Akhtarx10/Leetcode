86d46066db7a4af0e229abba253d6eb2494f1e50
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-03-02 21:00:56 +0530

    Time: 0 ms (100%), Space: 45.3 MB (61.98%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -1,32 +1,28 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        int i=0,j=0;
-
-        while(i < nums1.length && j < nums2.length){
-            if(nums1[i][0] == nums2[j][0]){
-                ans.add(Arrays.asList(Math.min(nums1[i][0], nums2[j][0]), nums1[i][1] + nums2[j][1]));
+        int n1=nums1.length,n2=nums2.length,i=0,j=0;
+        ArrayList<int []>ans=new ArrayList();
+        while(i<n1&& j<n2){
+            if(nums1[i][0]==nums2[j][0]){
+                ans.add(new int[]{nums1[i][0],nums1[i][1]+nums2[j][1]});
                 i++;
-                j++;
-            }else if(nums1[i][0] < nums2[j][0]){
-                ans.add(Arrays.asList(nums1[i][0], nums1[i][1]));
-                i++;
-            }else{
-                ans.add(Arrays.asList(nums2[j][0], nums2[j][1]));
                 j++;
             }
-        } 
-        while (i < nums1.length) {
-            ans.add(Arrays.asList(nums1[i][0], nums1[i][1]));
-            i++;
+            else if(nums1[i][0]<nums2[j][0]){
+                ans.add(nums1[i]);
+                i++;
+            }
+            else{
+                ans.add(nums2[j]);
+                j++;
+            }
         }
-
-        while (j < nums2.length) {
-            ans.add(Arrays.asList(nums2[j][0], nums2[j][1]));
-            j++;
+        while(i<n1){
+            ans.add(nums1[i++]);
         }
-
-        return ans.stream().map(l -> l.stream().mapToInt(Integer::intValue).toArray()).toArray(int[][]::new);
+        while(j<n2){
+            ans.add(nums2[j++]);
+        }
+        return ans.toArray(new int[ans.size()][]);
     }
 }
