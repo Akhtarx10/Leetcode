a98df75863e38f064ae94dda690203f7edba26ca
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-03-13 18:57:43 +0530

    Time: 3 ms (100%), Space: 129.3 MB (9.06%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
@@ -1,38 +1,26 @@
 class Solution {
-    private int n;
-    private int[] nums;
-    private int[][] queries;
-
     public int minZeroArray(int[] nums, int[][] queries) {
-        this.nums = nums;
-        this.queries = queries;
-        n = nums.length;
-        int m = queries.length;
-        int l = 0, r = m + 1;
-        while (l < r) {
-            int mid = (l + r) >> 1;
-            if (check(mid)) {
-                r = mid;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return l > m ? -1 : l;
-    }
+        int n = nums.length, sum = 0, k = 0;
+        int[] differenceArray = new int[n + 1];
 
-    private boolean check(int k) {
-        int[] d = new int[n + 1];
-        for (int i = 0; i < k; ++i) {
-            int l = queries[i][0], r = queries[i][1], val = queries[i][2];
-            d[l] += val;
-            d[r + 1] -= val;
-        }
-        for (int i = 0, s = 0; i < n; ++i) {
-            s += d[i];
-            if (nums[i] > s) {
-                return false;
+        for (int index = 0; index < n; index++) {
+            while (sum + differenceArray[index] < nums[index]) {  
+                k++;  
+                if (k > queries.length) {  
+                    return -1;
+                }
+
+                int left = queries[k - 1][0];  
+                int right = queries[k - 1][1];  
+                int val =  queries[k - 1][2];  
+
+                if (right >= index) {  
+                    differenceArray[Math.max(left, index)] += val;  
+                    differenceArray[right + 1] -= val;  
+                }
             }
+            sum += differenceArray[index];  
         }
-        return true;
+        return k;
     }
 }
