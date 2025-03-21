6d1416abaed686c0d8c09fcf9cb030e24a624074
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-03-21 21:17:41 +0530

    Time: 0 ms (100%), Space: 46.2 MB (96.25%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
@@ -1,40 +1,58 @@
+import java.util.*;
+
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        Map<String, List<String>> graph = new HashMap<>();
-        Map<String, Integer> indegreeMap = new HashMap<>();
-      
-        for (int i = 0; i < recipes.length; ++i) {
-            for (String ingredient : ingredients.get(i)) {
-                graph.computeIfAbsent(ingredient, k -> new ArrayList<>()).add(recipes[i]);
+        return new AbstractList<String>() {
+            List<String> result;
+
+            @Override
+            public String get(int index) {
+                return result.get(index);
             }
-            indegreeMap.put(recipes[i], ingredients.get(i).size());
-        }
-      
-        Deque<String> queue = new ArrayDeque<>();
-      
-        for (String supply : supplies) {
-            queue.offer(supply);
-        }
-      
-        List<String> availableRecipes = new ArrayList<>();
-      
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-          
-            for (int i = 0; i < size; ++i) {
-                String ingredient = queue.pollFirst();
-              
-                for (String recipe : graph.getOrDefault(ingredient, Collections.emptyList())) {
-                    indegreeMap.put(recipe, indegreeMap.get(recipe) - 1);
-                  
-                    if (indegreeMap.get(recipe) == 0) {
-                        availableRecipes.add(recipe);
-                        queue.offer(recipe);
+
+            @Override
+            public int size() {
+                if (result == null) {
+                    init();
+                }
+                return result.size();
+            }
+
+            private void init() {
+                Set<String> supplieSets = new HashSet<>(Arrays.asList(supplies));
+
+                Map<String, List<String>> graph = new HashMap<>();
+                Map<String, Integer> inDegreeMap = new HashMap<>();
+                for (int i = 0; i < recipes.length; i++) {
+                    for (String ingredient : ingredients.get(i)) {
+                        if (!supplieSets.contains(ingredient)) {
+                            graph.putIfAbsent(ingredient, new ArrayList<>());
+                            graph.get(ingredient).add(recipes[i]);
+                            inDegreeMap.merge(recipes[i], 1, Integer::sum);
+                        }
+                    }
+                }
+                Deque<String> deque = new ArrayDeque<>();
+                for (String recipe : recipes) {
+                    if (inDegreeMap.getOrDefault(recipe, 0) == 0) {
+                        deque.offer(recipe);
+                    }
+                }
+                result = new ArrayList<>();
+                while (!deque.isEmpty()) {
+                    String current = deque.poll();
+                    result.add(current);
+                    List<String> child = graph.get(current);
+                    if (child != null) {
+                        for (String chd : child) {
+                            inDegreeMap.merge(chd, -1, Integer::sum);
+                            if (inDegreeMap.get(chd) == 0) {
+                                deque.offer(chd);
+                            }
+                        }
                     }
                 }
             }
-        }
-      
-        return availableRecipes;
+        };
     }
 }
