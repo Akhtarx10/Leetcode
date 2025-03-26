bdd42dd3d7b88039524dc0b00573ac7c0f335cb1
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-03-26 22:10:56 +0530

    Time: 3 ms (100%), Space: 64.4 MB (99.5%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,21 +1,46 @@
 class Solution {
-  public int minOperations(int[][] grid, int x) {
-    final int m = grid.length;
-    final int n = grid[0].length;
-    int[] arr = new int[m * n];
-    for (int i = 0; i < m; ++i)
-      for (int j = 0; j < n; ++j)
-        arr[i * n + j] = grid[i][j];
-    if (Arrays.stream(arr).anyMatch(a -> (a - arr[0]) % x != 0))
-      return -1;
+    public int minOperations(int[][] grid, int x) {
 
-    int ans = 0;
+        int m = grid.length, n = grid[0].length;
 
-    Arrays.sort(arr);
+        int len = m * n;
+        if(len < 2) return 0;
+        
+        int[] count = new int[10001];
 
-    for (final int a : arr)
-      ans += Math.abs(a - arr[arr.length / 2]) / x;
+        int totalSum = 0;
+        int r = grid[0][0] % x;
+        for(int i = 0; i < m; ++i){
+            for(int j = 0; j < n; ++j){
+                int v = grid[i][j];
+                if(v % x != r) return -1;
+                count[v]++;
+            } 
+        }
+        
+        len = (len + 1) / 2;
+        int total = 0;
+        int median = 0;
 
-    return ans;
-  }
+        for(int v = 0; v < count.length; ++v){
+            if(count[v] == 0) continue;
+            total += count[v];
+            if(total >= len){
+                median = v;
+                break;
+            }
+        }
+        
+        int ans = 0;
+        for(int v = 0; v <= median; ++v){
+            if(count[v] == 0) continue;
+            ans += count[v] * (median - v) / x;
+        }
+
+        for(int v = median + 1; v < count.length; ++v){
+            if(count[v] == 0) continue;
+            ans += count[v] * (v - median) / x;
+        }        
+        return ans;
+    }
 }
