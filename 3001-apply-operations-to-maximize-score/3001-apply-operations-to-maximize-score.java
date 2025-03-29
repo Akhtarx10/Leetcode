ef12dc3e963fc11bbe678675773ff5105b5eb026
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-03-29 22:36:43 +0530

    Time: 120 ms (96.83%), Space: 57.1 MB (100%) - LeetHub

--- 3001-apply-operations-to-maximize-score/3001-apply-operations-to-maximize-score.java
+++ 3001-apply-operations-to-maximize-score/3001-apply-operations-to-maximize-score.java
@@ -1,97 +1,97 @@
 class Solution {
-  public int maximumScore(List<Integer> nums, int k) {
-    final int n = nums.size();
-    final int mx = Collections.max(nums);
-    final int[] minPrimeFactors = sieveEratosthenes(mx + 1);
-    final int[] primeScores = getPrimeScores(nums, minPrimeFactors);
-    int ans = 1;
-    // left[i] := the next index on the left (if any)
-    //            s.t. primeScores[left[i]] >= primeScores[i]
-    int[] left = new int[n];
-    Arrays.fill(left, -1);
-    // right[i] := the next index on the right (if any)
-    //             s.t. primeScores[right[i]] > primeScores[i]
-    int[] right = new int[n];
-    Arrays.fill(right, n);
-    Deque<Integer> stack = new ArrayDeque<>();
+    public static final int MOD = (int)1e9 + 7;
+    public int maximumScore(List<Integer> nums, int k) {
+        int n = nums.size(), max = 0;
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) arr[i] = nums.get(i);
+        for(int num : arr) max = Math.max(max, num);
+        
+        int[] primeScores = getPrimeScores(arr, max);
+        
+        int[] multiplierCnt = new int[max + 1]; //count how many different subarrays exist where this will be chosen multtiplier (i.e. has greatest prime score and is greatest num amoung those with such a score)
+        int[] greaterThanLeft = new int[n]; //gives tthe rightmost index tto the left of an element with greatter prime score/equal score and greater value to current
+        int[] greaterThanRight = new int[n]; //index of lefttmostt element to the right of curr with greater score 
+        greaterThanLeft[0] = -1;
+        greaterThanRight[n-1] = n;
+        for(int i = 1; i < n; i++) {
+            int left = i-1;
+            while(left >= 0 && (primeScores[arr[i]] > primeScores[arr[left]])) left = greaterThanLeft[left];
+            greaterThanLeft[i] = left;
 
-    // Find the next indices on the left where `primeScores` are greater or equal.
-    for (int i = n - 1; i >= 0; --i) {
-      while (!stack.isEmpty() && primeScores[stack.peek()] <= primeScores[i])
-        left[stack.pop()] = i;
-      stack.push(i);
+            int right = n-i;
+            while(right < n && (primeScores[arr[n-i-1]] >= primeScores[arr[right]])) right = greaterThanRight[right];
+            greaterThanRight[n-i-1] = right;
+        }
+
+        for(int i = 0; i < n; i++) {
+            /*can expand array from (left, right) (exclusive on both) so max array where this is max score element is sz = right-left-1, and sz gives total number of subarrays = sz*(sz+1)/2 in range
+            however, need center in so subtract all suubarrays without center = subtract triangular of i-left-1 and right-i-1*/
+            int sz = greaterThanRight[i]-greaterThanLeft[i]-1, leftSz = i-greaterThanLeft[i]-1, rightSz = greaterThanRight[i]-i-1;
+            long subarrays = ((sz*(sz+1L))/2 - (leftSz*(leftSz+1L))/2 - (rightSz*(rightSz+1L))/2);
+            multiplierCnt[arr[i]] = (int)Math.min(k, multiplierCnt[arr[i]] + subarrays); //add to the cnt, capping cnt as k as will never use > k multipliers
+            
+        }
+
+
+        long res = 1;
+        for(int mult = max; mult > 0; mult--) {
+            if(multiplierCnt[mult] == 0) continue;
+            if(multiplierCnt[mult] >= k) {
+                res = (res * modExp(mult, k)) % MOD;
+                break;
+            } else {
+                res = (res * modExp(mult, multiplierCnt[mult])) % MOD; 
+                k -= multiplierCnt[mult];
+            }
+        }
+
+        return (int)res;
     }
 
-    stack.clear();
+    private static int[] getPrimeScores(int[] nums, int max) {
+        int[] spf = sieveSmallestPFactors(max);
+        int[] primeScores = new int[max + 1];
+        for(int num : nums) {
+            if(primeScores[num] != 0) continue; //dupe value with already computed prime score
+            int x = num;
+            while(x > 1) {
+                primeScores[num]++;
+                int p = spf[x];
+                while(spf[x] == p) x /= p;
+            }
+        }
 
-    // Find the next indices on the right where `primeScores` are greater.
-    for (int i = 0; i < n; ++i) {
-      while (!stack.isEmpty() && primeScores[stack.peek()] < primeScores[i])
-        right[stack.pop()] = i;
-      stack.push(i);
+        return primeScores;
     }
 
-    Pair<Integer, Integer>[] numAndIndexes = new Pair[n];
+    private static int[] sieveSmallestPFactors(int lim) {
+        int[] spf = new int[lim + 1]; //smallest prime factor
+        boolean stopPostItr = false;
+        for(int i = 3; i <= lim; i += 2) {
+            spf[i-1] = 2;
+            if(spf[i] != 0) continue;
+            spf[i] = i;
 
-    for (int i = 0; i < n; ++i)
-      numAndIndexes[i] = new Pair<>(nums.get(i), i);
+            if(stopPostItr) continue;
+            stopPostItr = i*i > lim;
 
-    Arrays.sort(numAndIndexes,
-                Comparator.comparing(Pair<Integer, Integer>::getKey, Comparator.reverseOrder())
-                    .thenComparingInt(Pair<Integer, Integer>::getValue));
-
-    for (Pair<Integer, Integer> numAndIndex : numAndIndexes) {
-      final int num = numAndIndex.getKey();
-      final int i = numAndIndex.getValue();
-      // nums[i] is the maximum value in the range [left[i] + 1, right[i] - 1]
-      // So, there are (i - left[i]) * (right[i] - 1) ranges where nums[i] will
-      // be chosen.
-      final long rangeCount = (long) (i - left[i]) * (right[i] - i);
-      final long actualCount = Math.min(rangeCount, (long) k);
-      k -= actualCount;
-      ans = (int) ((1L * ans * modPow(num, actualCount)) % MOD);
+            for(int j = i*i; j <= lim; j += 2*i) {
+                if(spf[j] == 0) spf[j] = i;
+            }
+        }
+        if(lim % 2 == 0) spf[lim] = 2;
+        return spf;
     }
 
-    return ans;
-  }
-
-  private static final int MOD = 1_000_000_007;
-
-  private long modPow(long x, long n) {
-    if (n == 0)
-      return 1;
-    if (n % 2 == 1)
-      return x * modPow(x, n - 1) % MOD;
-    return modPow(x * x % MOD, n / 2);
-  }
-
-  // Gets the minimum prime factor of i, where 1 < i <= n.
-  private int[] sieveEratosthenes(int n) {
-    int[] minPrimeFactors = new int[n + 1];
-    for (int i = 2; i <= n; ++i)
-      minPrimeFactors[i] = i;
-    for (int i = 2; i * i < n; ++i)
-      if (minPrimeFactors[i] == i) // `i` is prime.
-        for (int j = i * i; j < n; j += i)
-          minPrimeFactors[j] = Math.min(minPrimeFactors[j], i);
-    return minPrimeFactors;
-  }
-
-  private int[] getPrimeScores(List<Integer> nums, int[] minPrimeFactors) {
-    int[] primeScores = new int[nums.size()];
-    for (int i = 0; i < nums.size(); ++i)
-      primeScores[i] = getPrimeScore(nums.get(i), minPrimeFactors);
-    return primeScores;
-  }
-
-  private int getPrimeScore(int num, int[] minPrimeFactors) {
-    Set<Integer> primeFactors = new HashSet<>();
-    while (num > 1) {
-      final int divisor = minPrimeFactors[num];
-      primeFactors.add(divisor);
-      while (num % divisor == 0)
-        num /= divisor;
+    private static long modExp(int base, int exp) {
+        long multiplier = base, res = 1;
+        while(exp > 0) {
+            if((exp & 1) == 1) res = (res * multiplier) % MOD;
+            multiplier = (multiplier * multiplier) % MOD;
+            exp >>= 1;
+        }
+        return res;
     }
-    return primeFactors.size();
-  }
+
+
 }
