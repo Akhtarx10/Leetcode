aad7f2460daea17a001fa05828daf4104c7a50ee
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-04-01 17:39:06 +0530

    Time: 1 ms (100%), Space: 108 MB (85%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.java
@@ -1,17 +1,32 @@
 class Solution {
-  public long mostPoints(int[][] questions) {
-    final int n = questions.length;
-    // dp[i] := the maximum points starting from questions[i]
-    long[] dp = new long[n + 1];
+    private long calcMaxPoints(int[][] questions, int index, int n, long[] dp) {
+        if (index == n) return 0;
+        if (dp[index] != -1) return dp[index];
+        long take = questions[index][0];
+        int newIndex = index + questions[index][1] + 1;
+        if (newIndex < n) take += calcMaxPoints(questions, index + questions[index][1] + 1, n, dp);
+        long notTake = calcMaxPoints(questions, index + 1, n, dp);
 
-    for (int i = n - 1; i >= 0; --i) {
-      final int points = questions[i][0];
-      final int brainpower = questions[i][1];
-      final int nextIndex = i + brainpower + 1;
-      final long nextPoints = nextIndex < n ? dp[nextIndex] : 0;
-      dp[i] = Math.max(points + nextPoints, dp[i + 1]);
+        return dp[index] = Math.max(take, notTake);
     }
+    static {
+        int[][] arr = {{3,2},{4,3},{4,4},{2,5}};
+        for (int i = 0 ; i < 100 ; i++) mostPoints(arr);
+    }
+    public static long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n];
+        // Arrays.fill(dp, -1);
+        // return calcMaxPoints(questions, 0, n, dp);
+        dp[n - 1] = questions[n - 1][0];
+        for (int i = n - 2 ; i >= 0 ; i--) {
+            long take = questions[i][0];
+            int newIndex = i + questions[i][1] + 1;
+            take += (newIndex < n) ? dp[newIndex] : 0;
+            long notTake = dp[i + 1];
+            dp[i] = Math.max(take, notTake);
+        }
 
-    return dp[0];
-  }
+        return dp[0];
+    }
 }
