8ad8bce040833baf8312e09df864fcf682521dd4
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-04-07 00:06:11 +0530

    Time: 0 ms (100%), Space: 40.8 MB (78.91%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.java
@@ -1,5 +1,21 @@
 class Solution {
-  public int subsetXORSum(int[] nums) {
-    return Arrays.stream(nums).reduce((a, b) -> a | b).getAsInt() << nums.length - 1;
-  }
+    int ans = 0;
+
+    public int subsetXORSum(int[] nums) {
+        if (nums.length == 1)
+            ans = nums[0];
+        else {
+            xor(0, nums, 0, nums.length - 1);
+        }
+        return ans;
+    }
+
+    private void xor(int X, int[] nums, int i, int j) {
+        if (i > j)
+            return;
+
+        ans += X ^ nums[i];
+        xor(X ^ nums[i], nums, i + 1, j);
+        xor(X, nums, i + 1, j);
+    }
 }
