043e7a14117648e5fdcc18686fb3c55e7592f900
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-04-15 21:12:42 +0530

    Time: 10 ms (100%), Space: 57.8 MB (61.9%) - LeetHub

--- 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.java
+++ 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.java
@@ -1,67 +1,61 @@
-class FenwickTree {
-  public FenwickTree(int n) {
-    sums = new int[n + 1];
-  }
-
-  public void add(int i, int delta) {
-    while (i < sums.length) {
-      sums[i] += delta;
-      i += lowbit(i);
+class Solution 
+{
+    public long goodTriplets(int[] nums1, int[] nums2) 
+    {
+        int n = nums1.length;
+        int[] pos2 = new int[n], reversedIndexMapping = new int[n];
+        
+        for (int i = 0; i < n; i++) 
+        {
+            pos2[nums2[i]] = i;
+        }
+        for (int i = 0; i < n; i++) 
+        {
+            reversedIndexMapping[pos2[nums1[i]]] = i;
+        }
+        
+        FenwickTree tree = new FenwickTree(n);
+        long res = 0;
+        for (int value = 0; value < n; value++) 
+        {
+            int pos = reversedIndexMapping[value];
+            int left = tree.query(pos);
+            tree.update(pos, 1);
+            int right = (n - 1 - pos) - (value - left);
+            res += (long) left * right;
+        }
+        return res;
     }
-  }
 
-  public int get(int i) {
-    int sum = 0;
-    while (i > 0) {
-      sum += sums[i];
-      i -= lowbit(i);
+    private class FenwickTree 
+    {
+        private int[] tree;
+
+        public FenwickTree(int size) 
+        {
+            this.tree = new int[size + 1];
+        }
+
+        public void update(int index, int delta) 
+        {
+            index++;
+            while (index < tree.length) 
+            {
+                this.tree[index] += delta;
+                index += index & -index;
+            }
+        }
+
+        public int query(int index) 
+        {
+            index++;
+            int res = 0;
+            while (index > 0) 
+            {
+                res += this.tree[index];
+                index -= index & -index;
+            }
+            return res;
+        }
     }
-    return sum;
-  }
-
-  private int[] sums;
-
-  private static int lowbit(int i) {
-    return i & -i;
-  }
 }
-
-class Solution {
-  public long goodTriplets(int[] nums1, int[] nums2) {
-    final int n = nums1.length;
-    long ans = 0;
-    Map<Integer, Integer> numToIndex = new HashMap<>();
-    int[] arr = new int[n];
-    // leftSmaller[i] := the number of arr[j] < arr[i], where 0 <= j < i
-    int[] leftSmaller = new int[n];
-    // rightLarger[i] := the number of arr[j] > arr[i], where i < j < n
-    int[] rightLarger = new int[n];
-    FenwickTree tree1 = new FenwickTree(n); // Calculates `leftSmaller`.
-    FenwickTree tree2 = new FenwickTree(n); // Calculates `rightLarger`.
-
-    for (int i = 0; i < n; ++i)
-      numToIndex.put(nums1[i], i);
-
-    // Remap each number in `nums2` to the according index in `nums1` as `arr`.
-    // So the problem is to find the number of increasing tripets in `arr`.
-    for (int i = 0; i < n; ++i)
-      arr[i] = numToIndex.get(nums2[i]);
-
-    for (int i = 0; i < n; ++i) {
-      leftSmaller[i] = tree1.get(arr[i]);
-      tree1.add(arr[i] + 1, 1);
-    }
-
-    for (int i = n - 1; i >= 0; --i) {
-      rightLarger[i] = tree2.get(n) - tree2.get(arr[i]);
-      tree2.add(arr[i] + 1, 1);
-    }
-
-    for (int i = 0; i < n; ++i)
-      ans += (long) leftSmaller[i] * rightLarger[i];
-
-    return ans;
-  }
-}
-
-
