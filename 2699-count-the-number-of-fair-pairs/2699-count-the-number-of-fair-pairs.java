baeac0e98a594a3f35a11538b34e16b1dfc82ed6
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-04-19 12:50:45 +0530

    Time: 23 ms (100%), Space: 57.6 MB (30.12%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.java
@@ -1,18 +1,18 @@
-class Solution {
-  public long countFairPairs(int[] nums, int lower, int upper) {
-    // nums[i] + nums[j] == nums[j] + nums[i], so the condition that i < j
-    // degrades to i != j and we can sort the array.
-    Arrays.sort(nums);
-    return countLess(nums, upper) - countLess(nums, lower - 1);
-  }
+import java.util.Arrays;
 
-  private long countLess(int[] nums, int sum) {
-    long res = 0;
-    for (int i = 0, j = nums.length - 1; i < j; ++i) {
-      while (i < j && nums[i] + nums[j] > sum)
-        --j;
-      res += j - i;
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        return count(nums, upper) - count(nums, lower - 1);
     }
-    return res;
-  }
+
+    private long count(int[] nums, int target) {
+        long res = 0;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] > target) right--;
+            else res += right - left++;
+        }
+        return res;
+    }
 }
