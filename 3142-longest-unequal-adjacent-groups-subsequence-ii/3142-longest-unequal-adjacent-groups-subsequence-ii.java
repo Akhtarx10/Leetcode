672f3ca111d9e86941208557db3ea8340c84a4fc
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-05-16 22:20:53 +0530

    Time: 13 ms (97.56%), Space: 45.9 MB (19.51%) - LeetHub

--- 3142-longest-unequal-adjacent-groups-subsequence-ii/3142-longest-unequal-adjacent-groups-subsequence-ii.java
+++ 3142-longest-unequal-adjacent-groups-subsequence-ii/3142-longest-unequal-adjacent-groups-subsequence-ii.java
@@ -1,47 +1,43 @@
 class Solution {
-  public List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
-    int n = words.length;
-    List<String> ans = new ArrayList<>();
-    int[] dp = new int[n];
-    Arrays.fill(dp, 1);
-    int[] prev = new int[n];
-    Arrays.fill(prev, -1);
+    public List<String> getWordsInLongestSubsequence(String[] words, int[] groups) {
+        int n = words.length;
+        Map<Long, List<Integer>> map = new HashMap();
+        
+        int[] ansNext = new int[n];
+        int[] lengths = new int[n];
+        
+        int ansIndex = 0;
 
-    for (int i = 1; i < n; ++i) {
-      for (int j = 0; j < i; ++j) {
-        if (groups[i] == groups[j])
-          continue;
-        if (words[i].length() != words[j].length())
-          continue;
-        if (hammingDist(words[i], words[j]) != 1)
-          continue;
-        if (dp[i] < dp[j] + 1) {
-          dp[i] = dp[j] + 1;
-          prev[i] = j;
+        Arrays.fill(ansNext, n);
+
+        for (int left = n - 1; left >= 0; left--) {
+            String word = words[left];
+            int len = word.length();
+            int res = 1;
+            long completeMask = 0l;
+            long[] masks = new long[len];
+            for (int i = 0; i < len; i++) {
+                completeMask |= masks[i] = (long)(word.charAt(i) - 'a' + 1) << (5 * i);
+            }
+            for (int i = 0; i < len; i++) {
+                long targetMask = completeMask ^ masks[i];
+                List<Integer> queue = map.computeIfAbsent​(targetMask, (j) -> new ArrayList());
+                for (int idx : queue) {
+                    if (res >= lengths[idx] + 1 || groups[idx] == groups[left] ) continue;
+                    res = lengths[idx] + 1;
+                    ansNext[left] = idx;
+                }
+                queue.add(left);
+            }
+            lengths[left] = res;
+            if (lengths[ansIndex] < res) ansIndex = left;
         }
-      }
+
+        List<String> ans = new ArrayList(lengths[ansIndex]);
+        for (int i = ansIndex; i < n; i = ansNext[i]) {
+            ans.add(words[i]);
+        }
+
+        return ans;
     }
-
-    int index = 0;
-    for (int i = 1; i < n; ++i) {
-      if (dp[i] > dp[index])
-        index = i;
-    }
-
-    while (index != -1) {
-      ans.add(words[index]);
-      index = prev[index];
-    }
-
-    Collections.reverse(ans);
-    return ans;
-  }
-
-  private int hammingDist(final String s1, final String s2) {
-    int dist = 0;
-    for (int i = 0; i < s1.length(); ++i)
-      if (s1.charAt(i) != s2.charAt(i))
-        ++dist;
-    return dist;
-  }
 }
