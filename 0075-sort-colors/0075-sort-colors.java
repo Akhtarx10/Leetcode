0b7c77b264dbae2f4f8d5e9efd4fe20009c8c284
Shoaib Akhtar <97195224+Akhtarx10@users.noreply.github.com>
2025-05-17 20:09:26 +0530

    Time: 0 ms (100%), Space: 42.3 MB (9.5%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        // int[] numsCopy = Arrays.copyOf(nums, nums.length);
+        // Arrays.sort(numsCopy);
+        // int[] ans = numsCopy;
+        // return ans;
+        Arrays.sort(nums);
+    }
+}
